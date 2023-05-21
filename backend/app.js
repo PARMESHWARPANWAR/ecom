1661a2f82aa33ab674617e6518771acab147ae32
Vipin Kumar <demonsorca@pop-os.localdomain>
2023-05-22 01:25:12 +0530

    changes for on heruko deployment

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,23 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-app.use(cors());
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
+const path = require("path");
+
 const errorMiddleware = require("./middleware/error");
 
-//config
-dotenv.config({ path: "backend/config/config.env" });
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-//Route Imports
+// Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
@@ -27,7 +28,13 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-//Middleware for Errors
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+// Middleware for Errors
 app.use(errorMiddleware);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
 const app = require("./app");
-
 const cloudinary = require("cloudinary");
-
-const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
-//Handling Uncaught Exception
+// Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
-  console.log(`Error : ${err.message}`);
+  console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Uncaught Exception`);
   process.exit(1);
 });
-//Config
 
-dotenv.config({ path: "backend/config/config.env" });
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
-//Connecting to database
+// Connecting to database
 connectDatabase();
 
 cloudinary.config({
@@ -24,13 +23,13 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is working on http://localhost:${process.env.PORT}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
-//Unhandled Promise Rejection
+// Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
-  console.log(`Error: $(err.message)`);
+  console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
   server.close(() => {
